ab39b7e769bd2c3f4e060c8c8d12e054fba62842
Carlos Rosquillas <crosquillas@gmail.com>
2023-10-09 20:26:14 -0700

    Add gravity to bird

--- lectures/fifty-bird/main.lua
+++ lectures/fifty-bird/main.lua
@@ -56,6 +56,9 @@ function love.update(dt)
 
 	-- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
 	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+	-- update bird based on gravity
+	bird:update(dt)
 end
 
 function love.draw()
