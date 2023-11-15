0fd95727159c8569ef7732220c42a4e861f5d4f6
Carlos Rosquillas <crosquillas@gmail.com>
2023-11-15 11:41:19 -0800

    Render pot above player in pot idle state

--- psets/zelda/src/states/entity/player/PlayerPotIdleState.lua
+++ psets/zelda/src/states/entity/player/PlayerPotIdleState.lua
@@ -7,14 +7,20 @@
 
 PlayerPotIdleState = Class{__includes = EntityIdleState}
 
+function PlayerPotIdleState:init(entity, dungeon)
+    self.entity = entity
+    self.dungeon = dungeon
+    self.pot = GameObject(GAME_OBJECT_DEFS['pot'], self.entity.x, self.entity.y)
+
+    EntityIdleState.init(self, entity)
+end
+
 function PlayerPotIdleState:enter(params)
     self.entity:changeAnimation('pot-idle-' .. self.entity.direction)
 
     -- render offset for spaced character sprite (negated in render function of state)
     self.entity.offsetY = 5
     self.entity.offsetX = 0
-
-    -- TODO: Render pot above player
 end
 
 function PlayerPotIdleState:update(dt)
@@ -24,9 +30,22 @@ function PlayerPotIdleState:update(dt)
     end
 
     -- TODO: Update pot position based on player position
+    self.pot.x = self.entity.x
+    self.pot.y = self.entity.y - 12
 
     -- TODO: Chuck pot when pressing return
     if love.keyboard.wasPressed('return') then
         self.entity:changeState('walk')
     end
+end
+
+function PlayerPotIdleState:render()
+    -- render player entity in pot idle state
+    local anim = self.entity.currentAnimation
+    love.graphics.draw(gTextures[anim.texture], gFrames[anim.texture][anim:getCurrentFrame()],
+        math.floor(self.entity.x - self.entity.offsetX), math.floor(self.entity.y - self.entity.offsetY))
+
+    -- render pot
+    love.graphics.draw(gTextures[self.pot.texture], gFrames[self.pot.texture][self.pot.frame],
+        self.pot.x, self.pot.y)
 end
--- psets/zelda/src/states/game/PlayState.lua
+++ psets/zelda/src/states/game/PlayState.lua
@@ -33,7 +33,7 @@ function PlayState:init()
         ['walk'] = function() return PlayerWalkState(self.player, self.dungeon) end,
         ['idle'] = function() return PlayerIdleState(self.player, self.dungeon) end,
         ['swing-sword'] = function() return PlayerSwingSwordState(self.player, self.dungeon) end,
-        ['pot-idle'] = function() return PlayerPotIdleState(self.player) end,
+        ['pot-idle'] = function() return PlayerPotIdleState(self.player, self.dungeon) end,
         ['pot-lift'] = function() return PlayerPotLiftState(self.player, self.dungeon) end,
         ['pot-walk'] = function() return PlayerPotWalkState(self.player, self.dungeon) end
     }
