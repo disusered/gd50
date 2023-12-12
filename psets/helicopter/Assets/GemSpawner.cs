6b9195946452ec59e98b0de9a2a4d613818e8033
Carlos Rosquillas <crosquillas@gmail.com>
2023-12-11 19:40:07 -0800

    Spawn gems worth 5 coins

---
+++ psets/helicopter/Assets/GemSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+
+    public GameObject[] prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // infinite gem spawning function, asynchronous
+        StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator SpawnGems()
+    {
+        while (true)
+        {
+            // number of gems we could spawn vertically
+            // rather than a range like gems, we just spawn 1 gem per row
+            // because they are worth more points
+            int gemsThisRow = 1;
+
+            // instantiate all gems in this row separated by some random amount of space
+            for (int i = 0; i < gemsThisRow; i++)
+            {
+                Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+            }
+            // pause 15-30 seconds until the next gem spawns
+            yield return new WaitForSeconds(Random.Range(15, 30));
+        }
+    }
+}
